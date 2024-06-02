fe1692dc9d345483ea0fd434915a7f322802ed44
Ainur Botagarova <botagarovaainura@gmail.com>
2024-06-01 23:00:38 -0500

    tests for the 'move' function were modified

--- test_game.py
+++ test_game.py
@@ -1,10 +1,22 @@
 import tic_tac_toe_1d
 
-def test_move_to_empty_space():
+def test_move_board_length():
     board = tic_tac_toe_1d.move('--------------------', 'x', 2)
     assert len(board) == 20
-    assert board.count('x') == 1
-    assert board.count('-') == 19
-    assert board[2] == 'x'
+    
 
-print('This file')
+def test_move_dash_count():
+    board = tic_tac_toe_1d.move('--------------------', 'x', 7)
+    assert board.count('-') == 19
+
+
+def test_move_mark_count():
+    board = tic_tac_toe_1d.move('--------------------', 'x', 3)
+    assert board.count('x') == 1
+
+
+def test_move_mark_position():
+    board = tic_tac_toe_1d.move ('--------------------', 'x', 1)
+    assert board[1] == 'x'
+
+
